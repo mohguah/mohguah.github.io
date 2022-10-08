30f0c629966202e39041a8cbbb2a2d8b43214033
simonhaughom <simon.haughom@gmail.com>
2022-10-08 22:14:34 +0200

    animated letters cont

--- react-portfolio/src/components/Home/index.js
+++ react-portfolio/src/components/Home/index.js
@@ -1,17 +1,24 @@
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import LogoTitle from '../../assets/images/logo-s.png'
+import AnimatedLetters from '../AnimatedLetters';
 import './index.scss';
 
 const Home = () => {
-
+    const [letterClass, setLetterClass] = useState('text-animate')
+    const nameArray = "imon".split("")
+    const jobArray = "Developer".split("")
+    
     return (
         <div className="container home-page">
             <div className="text-zone">
                 <h1>Hi, <br /> I'm
                 <img src={LogoTitle} alt="developer" />
-                imon
+                <AnimatedLetters letterClass={letterClass}
+                strArray={nameArray} idx={15} />
                 <br />
-                developer
+                <AnimatedLetters letterClass={letterClass}
+                strArray={jobArray} idx={15} />
                 </h1>
                 <h2>JavaScript / Python / R / Bioinformatics</h2>
                 <Link to="/contact" className='flat-button'>CONTACT ME</Link>
