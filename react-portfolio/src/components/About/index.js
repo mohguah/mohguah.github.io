2a53e75ea3a033c717bb4e138583abb0ea01f509
simonhaughom <simon.haughom@gmail.com>
2022-10-11 12:07:09 +0200

    about-page finished

--- react-portfolio/src/components/About/index.js
+++ react-portfolio/src/components/About/index.js
@@ -1,6 +1,5 @@
-import { faGitAlt, faHtml5, faJsSquare, faPython, faReact } from '@fortawesome/free-brands-svg-icons';
-import { solid, regular, brands, icon } from '@fortawesome/fontawesome-svg-core/import.macro'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { FaGitAlt, FaHtml5, FaJsSquare, FaPython, FaReact } from "react-icons/fa"
+import { SiRstudio } from "react-icons/si";
 import { useState } from 'react';
 import AnimatedLetters from '../AnimatedLetters';
 import './index.scss';
@@ -38,19 +37,22 @@ const About = () => {
                 <div className='stage-cube-cont'>
                     <div className='cubespinner'>
                         <div className='face1'>
-                            <FontAwesomeIcon icon={faReact} color='#5ED4F4' />
+                            <FaReact color='#5ED4F4' />
                         </div>
                         <div className='face2'>
-                            <FontAwesomeIcon icon={solid('fa-brands fa-r-project')} color='#276DC2' />
+                            <FaPython color='#FFD43B' />
                         </div>
                         <div className='face3'>
-                            <FontAwesomeIcon icon={faPython} color='#DD0031' />
+                            <SiRstudio color='#276DC2' />
                         </div>
                         <div className='face4'>
-                            <FontAwesomeIcon icon={faJsSquare} color='#EFD81D' />
+                            <FaJsSquare color='#EFD81D' />
                         </div>
                         <div className='face5'>
-                            <FontAwesomeIcon icon={faGitAlt} color='#EC4D28' />
+                            <FaGitAlt color='#EC4D28' />
+                        </div>
+                        <div className='face6'>
+                            <FaHtml5 color='#F06529' />
                         </div>
                     </div>
                 </div>
