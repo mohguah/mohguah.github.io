9965758ac1338707d2f6e69b8e04c8c2b7f08889
simonhaughom <simon.haughom@gmail.com>
2022-10-11 11:12:30 +0200

    about. installing babel package for icons

--- react-portfolio/src/components/About/index.js
+++ react-portfolio/src/components/About/index.js
@@ -1,19 +1,61 @@
+import { faGitAlt, faHtml5, faJsSquare, faPython, faReact } from '@fortawesome/free-brands-svg-icons';
+import { solid, regular, brands, icon } from '@fortawesome/fontawesome-svg-core/import.macro'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { useState } from 'react';
 import AnimatedLetters from '../AnimatedLetters';
 import './index.scss';
 
 
 
 const About = () => {
-    return(
-        <div className='container about-page'>
-            <div className='text-zone'>
-                <h1>
-                    <AnimatedLetters
-                        strArray={'About me'.split("")}
-                        idx={15} />
-                </h1>
+
+    return (
+        <>
+            <div className='container about-page'>
+                <div className='text-zone'>
+                    <h1>
+                        <AnimatedLetters
+                            strArray={['A', 'b', 'o', 'u', 't', ' ', 'm', 'e']}
+                            idx={15} />
+                    </h1>
+                    <p>
+                    "Lorem ipsum dolor sit amet, consectetur adipiscing
+                    elit, sed do eiusmod tempor incididunt ut labore
+                    et dolore magna aliqua. Ut enim ad minim veniam, quis 
+                    </p>
+                    <p>
+                    "Lorem ipsum dolor sit amet, consectetur adipiscing
+                    elit, sed do eiusmod tempor incididunt ut labore
+                    et dolore magna aliqua. Ut enim ad minim veniam, quis 
+                    </p>
+                    <p>
+                    "Lorem ipsum dolor sit amet, consectetur adipiscing
+                    elit, sed do eiusmod tempor incididunt ut labore
+                    et dolore magna aliqua. Ut enim ad minim veniam, quis 
+                    </p>
+                </div>
+
+                <div className='stage-cube-cont'>
+                    <div className='cubespinner'>
+                        <div className='face1'>
+                            <FontAwesomeIcon icon={faReact} color='#5ED4F4' />
+                        </div>
+                        <div className='face2'>
+                            <FontAwesomeIcon icon={solid('fa-brands fa-r-project')} color='#276DC2' />
+                        </div>
+                        <div className='face3'>
+                            <FontAwesomeIcon icon={faPython} color='#DD0031' />
+                        </div>
+                        <div className='face4'>
+                            <FontAwesomeIcon icon={faJsSquare} color='#EFD81D' />
+                        </div>
+                        <div className='face5'>
+                            <FontAwesomeIcon icon={faGitAlt} color='#EC4D28' />
+                        </div>
+                    </div>
+                </div>
             </div>
-        </div>
+        </>
     )
 }
 
--- react-portfolio/src/components/Home/index.js
+++ react-portfolio/src/components/Home/index.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import LogoTitle from '../../assets/images/logo-s.png'
 import AnimatedLetters from '../AnimatedLetters';
@@ -15,23 +15,23 @@ const Home = () => {
     //         setLetterClass('text-animate-hover')
     //     }, 4000)
     // }, [])
-    
+
     return (
         <div className="container home-page">
             <div className="text-zone">
                 <h1>
-                <span className={letterClass}>H</span>
-                <span className={`${letterClass} _12`}>i,</span>
-                <span className={`${letterClass} _13`}>I</span>
-                <span className={`${letterClass} _14`}>'m</span>
-                
-                <br /> I'm
-                <img src={LogoTitle} alt="developer" />
-                <AnimatedLetters letterClass={letterClass}
-                strArray={nameArray} idx={15} />
-                <br />
-                <AnimatedLetters letterClass={letterClass}
-                strArray={jobArray} idx={22} />
+                    <span className={letterClass}>H</span>
+                    <span className={`${letterClass} _12`}>i,</span>
+                    <span className={`${letterClass} _13`}>I</span>
+                    <span className={`${letterClass} _14`}>'m</span>
+
+                    <br /> I'm
+                    <img src={LogoTitle} alt="developer" />
+                    <AnimatedLetters letterClass={letterClass}
+                        strArray={nameArray} idx={15} />
+                    <br />
+                    <AnimatedLetters letterClass={letterClass}
+                        strArray={jobArray} idx={22} />
                 </h1>
                 <h2>JavaScript / Python / R / Bioinformatics</h2>
                 <Link to="/contact" className='flat-button'>CONTACT ME</Link>
